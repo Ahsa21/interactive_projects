fefd3e8e0894ade6cb78231964ff2cdc9dc2b6b1
Ahmad <Ahmadsalouka@gmail.com>
2024-02-27 00:10:07 +0100

    add two data files and import some libraries
    - data.csv and Israel-Palestine.csv are two data-files that were added.
    - some libraries were imported in lib.py file.
    - some code added to the analysis.py file.

---
+++ analysis.py
@@ -0,0 +1,4 @@
+import lib
+
+df = lib.pd.read_csv("data.csv")
+df2 = lib.pd.read_csv("Israel-Palestine.csv")
---
+++ lib.py
@@ -0,0 +1,4 @@
+import os
+import matplotlib.pyplot as mpl
+import pandas as pd
+import numpy as np
